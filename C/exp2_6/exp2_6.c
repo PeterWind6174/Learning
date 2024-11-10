b226f8af938464f1fda44cc1c8fa41ba7ade1a1f
YUHANG TU <35597627+PeterWind6174@users.noreply.github.com>
2024-11-10 13:13:50 +0000

    exp2_6

---
+++ C/exp2_6/exp2_6.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    double a, b, c, tmp;
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if (a > b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    if (a > c)
+    {
+        tmp = a;
+        a = c;
+        c = tmp;
+    }
+    if (b > c)
+    {
+        tmp = b;
+        b = c;
+        c = tmp;
+    }
+    if(a + b <= c)
+    {
+        printf("Not a triangle\n");
+        return 0;
+    }
+    if (a == b && b == c && a == c)
+        printf("Equilateral triangle\n");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles triangle\n");
+    else if (a * a + b * b == c * c)
+    {
+        if (a == b || b == c || a == c)
+            printf("Isosceles right triangle\n");
+        else
+            printf("Right-angled triangle\n");
+    }
+    else
+        printf("Scalene triangle\n");
+}
