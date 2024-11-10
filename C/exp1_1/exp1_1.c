9d6f8936db6ce4ad3c45de40e3bfde6a8d2b433c
YUHANG TU <35597627+PeterWind6174@users.noreply.github.com>
2024-11-10 12:33:31 +0000

    exp1_1

---
+++ C/exp1_1/exp1_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,sum,diference,product;
+    //int quotient;
+    double quotient,ave;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    diference = a - b;
+    product = a * b;
+    //quotient = a / b;
+    quotient = a * 1.0 / b;
+    //ave = (a + b) / 2;
+    ave = (a + b) / 2.0;
+    printf("Sum = %d\n", sum);
+    printf("Diference = %d\n", diference);
+    printf("Product = %d\n", product);
+    //printf("Quotient = %d\n", quotient);
+    printf("Quotient = %.2f\n", quotient);
+    //printf("Average = %d\n", ave);
+    printf("Average = %.2f\n", ave);
+    return 0;
+}
